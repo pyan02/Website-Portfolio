111b8ec374b0a81cc4c073bd04bb597b12b4be24
Paul Yan <paulyan14@gmail.com>
2020-01-19 16:29:11 -0500

    added images and changes to css

---
+++ app/script.js
@@ -0,0 +1,23 @@
+var i = 0;
+var images = [];
+var time = 5000;
+
+//list
+images[0] = '3D6_YANP.jpg';
+images[1] = 'ORTHO2_PY.jpg';
+images[2] = 'ORTHO4_PY.jpg';
+
+//change
+function changeImg(){
+    document.slide.src = images[i];
+
+    if(i<images.length-1){
+        i++;
+    } else {
+        i =0 ;
+    }
+
+    setTimeout("changeImg()",time);
+}
+
+window.onload = changeImg;
